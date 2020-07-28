958250484f484f3f5a29d67f3d59b33394e13712
vitor2233 <vitorguilherme2233@gmail.com>
2020-07-28 11:13:37 -0300

    Consuming git api

--- src/App.js
+++ src/App.js
@@ -1,51 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Routes from './Routes';
 import ProfilePhoto from './assets/eu.jpg';
 
 
-function App() {
+const App = () => {
+  const [publicRepos, setPublicRepos] = useState('0');
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/vitor2233')
+      .then(response => response.json())
+      .then(data => setPublicRepos(data.public_repos));
+  }, []);
+
   return (
     <div className="App">
       <div className="main">
       </div>
       <div className="profile">
-          <div className="about">
-            <img className="profile-img" src={ProfilePhoto} alt="Vítor Guilherme"/>
-            <div className="names">
+        <div className="about">
+          <img className="profile-img" src={ProfilePhoto} alt="Vítor Guilherme" />
+          <div className="names">
             <h2 className="desc">Vítor Guilherme</h2>
             <p className="desc">Desenvolvedor</p>
-            </div>
-          </div>
-          <hr/>
-          <div className="contact">
-            <p className="email"><a href="mailto:vitorguilherme2233@gmail.com">vitorguilherme2233@gmail.com</a></p>
-          </div>
-          <div className="options">
-            <ul>
-              <li>
-                <div className="projects-li">
-                  <p>12</p>
-                <a href="/">Projetos</a>
-                </div>
-              </li>
-              <li>
-              <a href="/courses">Cursos</a>
-              </li>
-              <li>
-              <a href="/formation">Formação</a>
-              </li>
-              <li>
-              <a href="/experience">Experiência</a>
-              </li>
-            </ul>
           </div>
         </div>
+        <hr />
+        <div className="contact">
+          <p className="email"><a href="mailto:vitorguilherme2233@gmail.com">vitorguilherme2233@gmail.com</a></p>
+        </div>
+        <div className="options">
+          <ul>
+            <li>
+              <div className="projects-li">
+                <p>{publicRepos}</p>
+                <a href="/">Projetos</a>
+              </div>
+            </li>
+            <li>
+              <a href="/courses">Cursos</a>
+            </li>
+            <li>
+              <a href="/formation">Formação</a>
+            </li>
+            <li>
+              <a href="/experience">Experiência</a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div className="content">
-        <Routes/>
+        <Routes />
       </div>
     </div>
   );
+
 }
 
 export default App;
--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 
-function Projects() {
+const Projects = () => {
+  const [repos, setRepos] = useState([]);
+  useEffect(() => {
+    fetch('https://api.github.com/users/vitor2233/repos')
+      .then(response => response.json())
+      .then(data => setRepos(data));
+  }, []);
   return (
     <div className="projects">
-        <p>projetos</p>
+      <ul>
+        {repos.map(repo =>
+          <li key={repo.id}>
+            <a href={repo.html_url}>{repo.name}</a>
+          </li>
+        )}
+      </ul>
     </div>
   );
 }
